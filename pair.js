23dad597a51150c7551cc6900ed10dc3f441eb48
LAZY <lazzybota@gmail.com>
2025-07-24 16:42:11 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,7 +81,7 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*⚡⚡⚡⚡LAZY Bot⚡⚡⚡⚡*\n\n👇👇👇👇👇👇👇👇👇👇👇\n\n*Your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*https://wa.me/94776294707*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/FXrkewcedXb5GOjLVKp7Yo*`;
+            const sid = `*⚡⚡⚡⚡⚡LAZY⚡⚡⚡⚡⚡*\n\n👇👇👇👇👇👇👇👇👇👇\n\n*Your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*https://wa.me/94776294707*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/FXrkewcedXb5GOjLVKp7Yo*`;
             const mg = `🟨 *Do not share this code to anyone* 🟨`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
